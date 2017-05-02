0b76e565adfcbe736f4c5fbfa3525753aad0d751
Hasnain <hasnainashraf12345@gmail.com>
2017-05-02 18:19:50 +0500

    Updated migrations

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Servic extends Model
+class Service extends Model
 {
     function gym(){
         return $this-> belongsTo(Gym :: class);
