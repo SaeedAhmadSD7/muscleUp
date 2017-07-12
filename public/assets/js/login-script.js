4fbf6a4bb778223b01bca95025f555007c15adb2
Faran <faran52000@yahoo.com>
2017-07-12 12:47:04 +0500

    Updated Script for Validation

--- public/assets/js/login-script.js
+++ public/assets/js/login-script.js
@@ -2,21 +2,6 @@ $(document).ready(function () {
 
     $('.user_login').validate({
         rules: {
-        //     firstname: "required",
-        //     lastname: "required",
-        //     username: {
-        //         required: true,
-        //         minlength: 2
-        //     },
-        //     password: {
-        //         required: true,
-        //         minlength: 5
-        //     },
-        //     confirm_password: {
-        //         required: true,
-        //         minlength: 5,
-        //         equalTo: "#password"
-        //     },
             email: {
                 required: true,
                 email: true
@@ -25,27 +10,14 @@ $(document).ready(function () {
                 required: true,
                 minlength: 8
             }
-        //     agree: "required"
         },
         messages: {
-        //     firstname: "Please enter your firstname",
-        //     lastname: "Please enter your lastname",
-        //     username: {
-        //         required: "Please enter a username",
-        //         minlength: "Your username must consist of at least 2 characters"
-        //     },
-        //     confirm_password: {
-        //         required: "Please provide a password",
-        //         minlength: "Your password must be at least 5 characters long",
-        //         equalTo: "Please enter the same password as above"
-        //     },
             email: "Please enter a valid email address",
             password: {
                 required: "Please enter your password",
                 minlength: "Your password must be atleast 8 characters long"
             }
 
-            //     agree: "Please accept our policy"
         },
         errorElement: "em",
         errorPlacement: function ( error, element ) {
