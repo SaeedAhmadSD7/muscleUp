b9f5b290b5fc3e9386b2616466e3dbd6eaac5ac6
Faran <faran52000@yahoo.com>
2017-07-26 13:40:34 +0500

    workout program

--- public/assets/js/program.js
+++ public/assets/js/program.js
@@ -1,38 +1,44 @@
 $(document).ready(function () {
 
-    // $('.toggle').find('.toggle-content').hide();
-    // $('.toggle-title').on('click', function () {
-    //     $(this).find('i').toggleClass('fa-plus fa-minus');
-    //     $(this).parent().find('.toggle-content').slideToggle();
-    // });
+    $('.day_detail-div').hide();
 
-    $('.tab-content').on('keypress','.phase_name',function () {
+    $('.toggle').on('click', function () {
+        $(this).find('i').toggleClass('icon-plus icon-minus');
+        $(this).closest('.day').siblings('.day_detail-div').slideToggle();
+    });
+
+    $('.tab-content').on('keypress','.phase_name, .day_name',function () {
         if($(this).val()) {
             $(this).siblings('.input-group-btn').children().prop('disabled',false);
         }
     });
 
-    $('.tab-content').on('focusout','.phase_name',function () {
+    $('.tab-content').on('focusout','.phase_name, .day_name',function () {
         if($(this).val() === '') {
             $(this).siblings('.input-group-btn').children().prop('disabled',true);
         }
     });
 
     $('.tab-content').on('click','.phase_detail',function () {
-        var phase_name_val = $(this).parent().siblings('.phase_name-div').find('.phase_name').val();
-        $(this).closest('.input-group').siblings('.modal').find('.phase_heading').html(phase_name_val);
-        $(this).closest('.input-group').siblings('.bs-example-modal-lg').modal({
+        var phase_name_val = $(this).parent().siblings('.phase_name').val();
+        $(this).closest('.input-group').siblings('.modal').find('.modal-title').html(phase_name_val);
+        $(this).closest('.input-group').siblings('.phase_details_modal').modal({
             keyboard: 'false',
             backdrop: 'static'
         })
     });
 
     $('.tab-content').on('click','.modal_close, .close',function () {
-            $(this).closest('.bs-example-modal-lg').modal('hide');
+            $(this).closest('.phase_details_modal').modal('hide');
     });
 
     $('.add-phase').on('click',function () {
-        var phase_clone = $('.phase:last').clone().insertAfter('.phase:last').find('input').val('').end().find('.phase_detail').prop('disabled',true);
+        $('.phase:last').clone().insertAfter('.phase:last').find('input').val('').end().find('.phase_detail').prop('disabled',true);
     });
+
+    $('.add-day').on('click',function () {
+        $('.day:last').clone().insertAfter('.day:last').find('input').val('').end().find('.day_detail').prop('disabled',true);
+    });
+
 
 });
--- resources/views/muscle-up-app/workout/workout-program.blade.php
+++ resources/views/muscle-up-app/workout/workout-program.blade.php
@@ -3,7 +3,6 @@
 @section('title','Workout program')
 
 @section('style-sheet')
-
     <link href="{{url('/admin-assets/elements/forms.css')}}" rel="stylesheet" type="text/css">
     <link href="{{url('/admin-assets/themes/components/border-radius.css')}}" rel="stylesheet" type="text/css">
     <link href="{{url('/admin-assets/widgets/modal/modal.css')}}" rel="stylesheet" type="text/css">
@@ -11,11 +10,6 @@
     <link href="{{url('/admin-assets/widgets/wizard/wizard.css')}}" rel="stylesheet" type="text/css">
     <link href="{{url('/admin-assets/elements/buttons.css')}}" rel="stylesheet" type="text/css">
     <link href="{{url('/admin-assets/css/workout.css')}}" rel="stylesheet" type="text/css">
-
-
-    {{--<link href="{{url('/assets/elements/panel-box.css')}}" rel="stylesheet" type="text/css">--}}
-
-
 @stop
 
 @section('content')
@@ -65,24 +59,50 @@
                                                     <button class="btn btn-blue-alt phase_detail" type="button" disabled><span class="input-group-btn">Add Details</span></button>
                                                 </span>
                                             </div>
-                                            <div aria-hidden="true" aria-labelledby="myLargeModalLabel" class="modal fade bs-example-modal-lg" role="dialog" tabindex="-1">
-                                                <div class="modal-dialog modal-lg">
+                                            <div aria-hidden="true" class="modal fade phase_details_modal" role="dialog" tabindex="-1">
+                                                <div class="modal-dialog">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
-                                                            <button aria-hidden="true" class="close" type="button">&times;</button>
-
-                                                            <h4 class="modal-title">Modal title</h4>
+                                                            <button aria-hidden="true" class="close" data-dismiss="modal" type="button">&times;</button>
+                                                            <h4 class="modal-title">Phase Name</h4>
                                                         </div>
 
 
                                                         <div class="modal-body">
-                                                            <p>Large modal content here ...</p>
+                                                            <div class="container-fluid">
+                                                                <div class="row">
+                                                                    <div class="form-group day">
+                                                                        <div class="col-sm-12">
+                                                                            <div class="input-group">
+                                                                                <input class="form-control day_name" name="" value="" type="text" placeholder="Enter Day Name...">
+                                                                                <span class="input-group-btn">
+                                                                                    <button class="btn btn-blue-alt day_detail toggle" type="button" disabled><span>Add Details  </span><i class="glyph-icon icon-plus"></i></button>
+                                                                                </span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group day_detail-div">
+                                                                        <div class="toggle-content">
+                                                                            <div class="form-group">
+                                                                                <div class="col-sm-6">
+                                                                                    <input class="form-control day_name" name="" value="" type="text" placeholder="Enter Day Name...">
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+
+                                                                    <div class="form-group">
+                                                                        <div class="col-sm-6">
+                                                                            <button class="btn btn-alt btn-hover btn-primary add-day" type="button"><span>Add Day</span><i class="glyph-icon icon-arrow-up"></i></button>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
                                                         </div>
 
 
                                                         <div class="modal-footer">
-                                                            <button class="btn btn-default modal_close" type="button">Close</button>
-                                                            <button class="btn btn-primary" type="button">Save changes</button>
+                                                            <button class="btn btn-default" data-dismiss="modal" type="button">Close</button> <button class="btn btn-primary" type="button">Save changes</button>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -141,5 +161,4 @@
     <script src="{{url('/admin-assets/widgets/button/button.js')}}" type="text/javascript"></script>
     <script src="{{url('/admin-assets/widgets/modal/modal.js')}}" type="text/javascript"></script>
     <script src="{{url('/assets/js/program.js')}}"></script>
-
 @stop
