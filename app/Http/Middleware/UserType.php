25228b7d1b0ba78da373913e4b7deb7678fbf51e
Faran <faran52000@yahoo.com>
2017-09-08 21:51:41 +0500

    Merge branch 'development' of gitlab.com:m.rashid.se/MuscleUp into development
    # Conflicts:
    #	resources/views/muscle-up-app/admin/inbox.blade.php

---
+++ app/Http/Middleware/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class UserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user() && Auth::user()->type == 'gym') {
+            return $next($request);
+        }
+        else if (Auth::user() && Auth::user()->type == 'admin') {
+            return $next($request);
+        }
+        else if (Auth::user() && Auth::user()->type == 'trainee') {
+            return $next($request);
+        }
+        else if (Auth::user() && Auth::user()->type == 'employee') {
+            return $next($request);
+        }
+        return redirect('/');
+    }
+}
