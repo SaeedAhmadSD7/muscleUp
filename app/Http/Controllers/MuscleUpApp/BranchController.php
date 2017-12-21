dc7856b70f3ab1e7b1f2b914196598f4eb825c99
Ahmad Saeed Gill <uandsaeed@gmail.com>
2017-12-21 22:39:35 +0500

    branch registration

--- app/Http/Controllers/MuscleUpApp/BranchController.php
+++ app/Http/Controllers/MuscleUpApp/BranchController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BranchCreateRequest;
 use Illuminate\Support\Facades\Auth;
+use \App\Utils\Globals\UserType;
 
 class BranchController extends Controller
 {
@@ -47,28 +48,33 @@ class BranchController extends Controller
 
     public function store(BranchCreateRequest $request){
 
+
+//dd(Auth::user()->id);
+        $branch = new Branch();
+        $branch->gym_id = Auth::user()->gym_id;
+//        $branch->branch_no = $request->branch_no;
+        $branch->title = $request->branch_name;
+//        $branch->email = $request->branch_email;
+//        $branch->phone_no = $request->branch_phone;
+        $branch->opening_time = $request->opening_time;
+        $branch->closing_time = $request->closing_time;
+        $branch->address = $request->branch_address;
+        $branch->save();
+
+        //        dd($request)->all;
         $user = new User();
+        $user->branch_id = $branch->id;
+        $user->gym_id = Auth::user()->gym_id;
         $user->first_name=$request->admin_name;
         $user->email = $request->admin_email;
 //        $password = str_random(8);
         $password = "asdf1234";
         $user->password = bcrypt($password);
-        $user->type = 'gym';
+        $user->type = UserType::ADMIN;
         $user->phone_number=$request->admin_phone;
 //        $user->address=$request->address;
         $user->save();
 
-
-        $branch = new Branch();
-        $branch->gym_id = Auth::user()->gym->id;
-//        $branch->branch_no = $request->branch_no;
-        $branch->title = $request->branch_name;
-//        $branch->email = $request->branch_email;
-        $branch->phone_no = $request->branch_phone;
-//        $branch->opening_time = $request->opening_time;
-//        $branch->clossing_time = $request->clossing_time;
-        $branch->address = $request->address;
-        $branch->save();
 
         dd("Success");
 
