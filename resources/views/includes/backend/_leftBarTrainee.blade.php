6a54d612b31f7b993b8035a75549ccc753736c70
Ahmad Saeed Gill <uandsaeed@gmail.com>
2018-03-12 23:02:40 +0500

    instructor edit

--- resources/views/includes/backend/_leftBarTrainee.blade.php
+++ resources/views/includes/backend/_leftBarTrainee.blade.php
@@ -1,10 +1,10 @@
 <li class="menu-element">
     <a href="{{route('traineeDashboard')}}" title="Trainee Dashboard"><i class="glyph-icon icon-linecons-tv"></i><span>Trainee Dashboard</span></a>
 </li>
-{!! $id = get_trainee_id() !!}
+{{--{!! $id = get_trainee_id() !!}--}}
 
 <li class="header"><span>Members Area</span></li>
-<li class="menu-element"><a href="{{route('trainee-personal-detail-edit',$id)}}"><i class="glyph-icon icon-linecons-tv"></i><span>Edit</span></a></li>
+<li class="menu-element"><a href="{{route('trainee-personal-detail-edit', get_trainee_id())}}"><i class="glyph-icon icon-linecons-tv"></i><span>Edit</span></a></li>
 <li class="menu-element"><a href="{{route('dietprogram-view')}}"><i class="glyph-icon icon-linecons-tv"></i><span>Diet Program</span></a></li>
 <li class="menu-element"><a href="{{route('workoutprogram-view')}}"><i class="glyph-icon icon-linecons-tv"></i><span>Workout Program</span></a></li>
 <li class="menu-element"><a href="{{route('trainee-activity')}}"><i class="glyph-icon icon-linecons-tv"></i><span>Progress</span></a></li>
